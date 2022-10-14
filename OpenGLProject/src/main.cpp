aeb98a5a50a7bba87dc92487dadb9cfdf80537ca
Виталий Черников <chernickovvit@yandex.ru>
2022-10-15 00:28:00 +0300

    Добавьте файлы проекта.

---
+++ OpenGLProject/src/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main() {
+	std::cout << "Hello, world!" << std::endl;
+
+	glm::vec4 vec(1.0f, 1.0f, 1.0f, 1.0f); // glm test
+	glm::mat4 trans = glm::mat4(1.0f); // identity matrix
+	trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f)); // translation
+	trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0)); // rotation
+	trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5)); // scaling
+	vec = trans * vec;
+	std::cout << vec.x << " " << vec.y << " " << vec.z << std::endl;
+
+	glfwInit();
+
+	// opengl version 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
+	// if window not created
+	if (window == NULL) {
+		std::cout << "Could not create window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// check initialize GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to ininitialize GLAD" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window)) {
+		// process inpot
+		processInput(window);
+
+		// render
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// send new frame to window
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
